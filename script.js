99b0cc5c6e67b4dee712f543a12bc9a505cc8c46
abhi-develope <prajapatiabhishek320@gmail.com>
2024-03-23 15:43:16 +0530

    project completed

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 let body = document.querySelector('body')
 body.onclick = function(e){
+    let images = [
+        'url("holi1.png")',
+        'url("holi2.png")',
+        'url("holi3.png")',
+        'url("holi4.png")',
+        'url("holi5.png")',
+        'url("holi6.png")',
+        'url("holi7.png")',
+        'url("holi8.png")',
+        'url("holi9.png")',
+        'url("holi10.png")',
+        'url("holi11.png")',
+        'url("holi12.png")',
+    ]
     let x = e.pageX - e.target.offsetLeft;
     let y = e.pageY - e.target.offsetTop;
 
@@ -7,9 +21,13 @@ body.onclick = function(e){
     splash.style.left = x + 'px';
     splash.style.top = y + 'px';
 
+    let bg = images[Math.floor(Math.random() * images.length)];
+
+    splash.style.backgroundImage = bg;
+
     this.appendChild(splash);
 
     setTimeout(()=> {
         splash.remove()
-    },4000)
+    },2000)
 }
